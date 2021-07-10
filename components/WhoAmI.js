2dafc50370e340d263260fa4a32850b59905707c
Jonas Alolino <jonasqalolino@gmail.com>
2021-07-10 22:22:41 +0800

    fix y spacing

--- components/WhoAmI.js
+++ components/WhoAmI.js
@@ -2,14 +2,15 @@ import Image from 'next/image';
 import profilePic from '../public/images/me.jpg';
 import Container from './Container';
 import ContainX from './ContainX';
+import wave from '../public/images/wave.png';
 
 const WhoAmI = () => {
 	return (
 		<section className='border-b-3'>
 			<Container>
-				<div className='relative w-full sm:flex sm:justify-between sm:h-440 md2:h-500 md2:flex md2:items-center'>
+				<div className='relative w-full py-10 sm:flex sm:justify-between sm:items-center sm:py-16 md2:h-500 md2:flex md2:items-center'>
 					<ContainX>
-						<div className='relative z-20 pt-10 sm:absolute sm:top-8 '>
+						<div className='relative z-20 sm:absolute sm:top-28 '>
 							<h1 className='text-5xl font-heading md:text-5.5xl'>
 								Jonas Alolino
 							</h1>
@@ -19,14 +20,19 @@ const WhoAmI = () => {
 							<p className='text-4xl text-right text-green-dark md:text-4.5xl '>
 								developer
 							</p>
+
+							<div className='hidden mt-8 ml-4 sm:block'>
+								<div className='flex items-center justify-center w-24 h-24 p-6 rounded-full border-3'>
+									<Image src={wave} alt='wave' />
+								</div>
+							</div>
 						</div>
 
-						<div className='flex items-center justify-center px-4 pt-4 pb-16 sm:w-full sm:justify-end sm:pb-8 xxs:pb-20 '>
-							<div className='flex items-center justify-center mt-4 mb-2 filter drop-shadow-lg h-320 w-260 xxs:w-280 xxs:mt-6 sm:mt-11 md:w-300 md2:w-320 md2:mb-8'>
+						<div className='flex items-center justify-center px-4 pt-4 pb-2.5 sm:w-full sm:justify-end sm:pb-8 xxs:pb-20 '>
+							<div className='flex items-center justify-center filter drop-shadow-lg w-260 xxs:w-280 xxs:mt-6 sm:mt-11 md:w-300 md2:w-320 md2:mb-8 opacity-10'>
 								<Image
 									src={profilePic}
 									alt='Picture of Me'
-									objectFit='contain'
 									priority
 									className='rounded'
 								/>
